5e2fae1e4a994a4d1152202f3f40076bddb50ae7
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2022-12-24 12:39:03 +0200

    add hooks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -7,78 +7,27 @@ import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
-class App extends Component  {
-    state = {
-        selectedChar: null
-    }
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-        })
-    }
-    class WhoAmI extends Component  {
-        constructor(props){
-          super(props);
-          this.state = {
-            years: 27,
-            text: '+++',
-            position:'' 
-          }
-          
-  nextYear = () =>{
-    this.setState(state =>({
-      years: state.years + 1
-    }))
-  }
-
-  commitInputChanges = (e, color) =>{
-    console.log(color);
-    this.setState({
-      position: e.target.value
-    })
-  }
+const App = () => {
+    const[selectedChar, setChar] = useState(null)
   
-  const Wrapper = styled.div`
-    width: 600px;
-    margin: 80px auto 0 auto
-  `;
+  const onCharSelected = (id) => {
+        setChar(id)
+    }
 
-const DynamicGreeting = (props) => {
-  return (
-    <div className={'mb-3 p-3 border border-' + props.color}>
-      {
-        React.Children.map(props.children, child =>{
-          return React.cloneElement(child, {className: 'shadow p-3 m-3 border rounded'})
-        })
-      }
-    </div> 
     
-  )
-  class Counter extends Component {
-    state = {
-      counter: 0
-    }
-    changeCounter = () => {
-      this.setState(({counter}) => ({
-        counter: counter +1
-      }))
-    }
-}
-        }
-    render() {
         return (
             <div className="app">
                 <AppHeader/>
                 <main>
                     <ErrorBoundary>
-                    <RandomChar/>
+                        <RandomChar/>
                     </ErrorBoundary>
                     <div className="char__content">
                         <ErrorBoundary>
-                        <CharList onCharSelected= {this.onCharSelected}/>
+                            <CharList onCharSelected={onCharSelected}/>
                         </ErrorBoundary>
                         <ErrorBoundary>
-                        <CharInfo charId = {this.state.selectedChar}/>
+                            <CharInfo charId={selectedChar}/>
                         </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
@@ -86,6 +35,6 @@ const DynamicGreeting = (props) => {
             </div>
         )
     }
-}
+
 
 export default App;
