c29a166736e46d0ba25e8e4ed1e0f6b8478bc0b2
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2022-12-30 20:16:04 +0200

    add useCounter

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,7 +6,47 @@ import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
+function useCounter(initial) {
+    const [counter, setCounter] = React.useState(initial);
+  
+    // Это вариант с запросом, чтобы он нормально работал после активации - уберите все props,
+    // которые приходят в компонент + аргумент initial поменяйте на 0 или null
+  
+    // React.useEffect(() => {
+    //     fetch('https://www.random.org/integers/?num=1&min=-50&max=50&col=1&base=10&format=plain&rnd=new')
+    //         .then(res => res.text())
+    //         .then(res => setCounter(res))
+    //         .catch(err => console.log(err))
+    // }, [])
+  
+    const incCounter = () => {
+      if (counter < 50) {
+        setCounter(counter => counter + 1)
+      }
+    } 
 
+    const decCounter = () => {
+      if (counter > -50) {
+        setCounter(counter => counter - 1)
+      }
+    }
+
+    const rndCounter = () => {
+      setCounter(+(Math.random() * (50 - -50) + -50).toFixed(0))
+    }
+
+    const resetCounter = () => {
+      setCounter(initial)
+    }
+    
+    return {
+      counter,
+      incCounter,
+      decCounter,
+      rndCounter,
+      resetCounter
+    }
+}
 function useInputWithValidate(initialValue) {
     const [value, setValue] = useState(initialValue);
 
