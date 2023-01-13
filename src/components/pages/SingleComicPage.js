2218bd0bd68c32fce0d15fb4164dc9796d751af2
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-01-13 17:34:48 +0200

    edit SingleComic

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -3,25 +3,63 @@ import { useState, useEffect } from 'react';
 import Spinner from '../spinner/Spinner';
 import Skeleton from '../skeleton/Skeleton';
 import ErrorMessage from '../errorMessage/errorMessage';
+import useMarvelService from '../../services/MarvelService';
 
 import './singleComicPage.scss';
 import xMen from '../../resources/img/x-men.png';
 
 const SingleComicPage = () => {
     const {comicId} = useParams();
+    const [comic, setComic] = useState(null);
+    const {loading, error, getComic, clearError} = useMarvelService();
     
+    
+    useEffect(() =>{
+        updateComic()
+       }, [comicId])
+      
+    
+    
+      
+       const updateComic = () => {
+                clearError();
+                getComic(comicId)
+                .then(onComicLoaded)
+              
+    
+            
+        }
+    
+      const  onComicLoaded = (char) => {
+           setComic(comic)
+        }
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error ||  !comic) ? <View char={comic}/> : null;
+    return (
+      <>
+      
+            {errorMessage}
+            {spinner}
+            {content}
+      </>
+    )
+}
+
+const View = ({comic}) => {
+    const {title, description, pageCount, thumbnail, language, price} = comic;
     return (
         <div className="single-comic">
-            <img src={xMen} alt="x-men" className="single-comic__img"/>
-            <div className="single-comic__info">
-                <h2 className="single-comic__name">X-Men: Days of Future Past</h2>
-                <p className="single-comic__descr">Re-live the legendary first journey into the dystopian future of 2013 - where Sentinels stalk the Earth, and the X-Men are humanity's only hope...until they die! Also featuring the first appearance of Alpha Flight, the return of the Wendigo, the history of the X-Men from Cyclops himself...and a demon for Christmas!?</p>
-                <p className="single-comic__descr">144 pages</p>
-                <p className="single-comic__descr">Language: en-us</p>
-                <div className="single-comic__price">9.99$</div>
-            </div>
-            <a href="#" className="single-comic__back">Back to all</a>
+        <img src={thumbnail} alt={title} className="single-comic__img"/>
+        <div className="single-comic__info">
+            <h2 className="single-comic__name">{title}</h2>
+            <p className="single-comic__descr">{description}</p>
+            <p className="single-comic__descr">{pageCount}</p>
+            <p className="single-comic__descr">Language: {language}</p>
+            <div className="single-comic__price">{price}</div>
         </div>
+        <Link to="/comics" className="single-comic__back">Back to all</Link>
+    </div>   
     )
 }
 
