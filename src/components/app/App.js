a91ba757ecb715ba08b4e8a24f902b38078601ae
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-01-21 23:35:46 +0200

    add new Container

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,20 +8,21 @@ InputComponent.contextType = dataContext;
  const Form =memo( (props) => {
 
     return (
-        <Container>
-            <form className="w-50 border mt-5 p-3 m-auto">
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <input value={props.mail.name} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com"/>
-                    </div>
+        
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
                     <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
-            </form>
-        </Container>
-    )
-}, propsCompare) 
+                        <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+                        <input value={this.props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com"/>
+                        </div>
+                        <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea value={this.props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+            </Container>
+        )
+    }, propsCompare) 
 const App = () => {
 
     return (
