c930c9e3bd80ad57ed4af1a93ee11921dcb663ef
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-01-23 22:40:35 +0200

    add InputComponent

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,25 +4,17 @@ import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
 
 
-InputComponent.contextType = dataContext;
- const Form =memo( (props) => {
+const InputComponent = () => {
 
-    return (
-        
-            <Container>
-                <form className="w-50 border mt-5 p-3 m-auto">
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                        <input value={this.props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com"/>
-                        </div>
-                        <div className="mb-3">
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea value={this.props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                </form>
-            </Container>
-        )
-    }, propsCompare) 
+    const context = useContext(dataContext);
+
+    return(
+    <input value={context.mail}
+     type="email"
+      className='form-control'
+       placeholder="name@example.com" />
+    )
+}
 const App = () => {
 
     return (
