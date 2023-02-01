377388f870932e1719438595a6519e257d978b71
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-02-01 16:42:44 +0200

    try Helmet

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import {  useState, useReducer } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { MainPage, ComicsPage, Page404, SingleComicPage, SinglePage} from "../pages";
+import { MainPage, ComicsPage, Page404, SingleComicPage} from "../pages";
 import AppHeader from "../appHeader/AppHeader";
 
 
@@ -15,7 +15,7 @@ const App = () => {
                         <Route  path="/" element = {<MainPage/>}/>
                         <Route  path="/comics" element = {<ComicsPage/>}/>   
                         <Route  path="/comics/:comicId" element = {<SingleComicPage/>}/>
-                        <Route  path="/comics/:id" element = {<SinglePage/>}/>
+                       
                         <Route path ='*' element={<Page404/>}/>
                         </Routes>
                       
