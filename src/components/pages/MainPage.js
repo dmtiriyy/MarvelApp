d4bcb51004af10a4bb90f8591d3ba4f49a7770b8
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-02-05 15:42:22 +0200

    edit finite-state machine

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,30 +1,30 @@
 import { useState } from "react";
 import { Helmet } from "react-helmet";
+
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import CharSearchForm from '../charSearchForm/CharSearchForm';
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import CharSearchForm from "../charSearchForm/CharSearchForm";
 
 import decoration from '../../resources/img/vision.png';
 
 const MainPage = () => {
 
     const [selectedChar, setChar] = useState(null);
-    const {loading, error, getComic, getCharacter, clearError, process, setProcess} = useMarvelService();
+
     const onCharSelected = (id) => {
         setChar(id);
     }
 
     return (
         <>
-         {setContent(process, Component, data)}
             <Helmet>
-    <meta
-        name="description"
-        content="Marvel information portal"
-    />
-        <title>Marvel information portal</title>
+                <meta
+                    name="description"
+                    content="Marvel information portal"
+                    />
+                <title>Marvel information portal</title>
             </Helmet>
             <ErrorBoundary>
                 <RandomChar/>
@@ -34,12 +34,12 @@ const MainPage = () => {
                     <CharList onCharSelected={onCharSelected}/>
                 </ErrorBoundary>
                 <div>
-                <ErrorBoundary>
-                    <CharInfo charId={selectedChar}/>
-                </ErrorBoundary>
-                <ErrorBoundary>
-                    <CharSearchForm/>
-                </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharSearchForm/>
+                    </ErrorBoundary>
                 </div>
             </div>
             <img className="bg-decoration" src={decoration} alt="vision"/>
